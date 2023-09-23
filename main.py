f63e3110a40678c0aec1299f8e83331994b67f47
Preet Sojitra <preet.dev373@gmail.com>
2023-09-23 22:21:13 +0530

    feat: convert hindi to pdf

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-import os
+# import os
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import logging
+import pdfkit
+
+# import docx
+# from docx2pdf import convert
+
 
 # import flask_cors
 from PyPDF2 import PdfReader
@@ -24,8 +29,71 @@ def read_file(file_path):
     print("Number of pages: ", number_of_pages)
     page = reader.pages[0]
     text = page.extract_text()
-    print("Extracted Text is: ", end="\n")
-    print(text)
+    # print("Extracted Text is: ", end="\n")
+    # print(text)
+
+
+def translate(text):
+    # Here we will pass to ML model
+
+    # For now we will return some dummy hindi paragraph
+    translated_text = "हिंदी हमारी राष्ट्रीय भाषा है। हमारे हिंदी भाषा कौशल को सीखना और सुधारना भारत के अधिकांश स्थानों में सेवा करने के लिए बहुत महत्वपूर्ण है। स्कूली दिनों से ही हम हिंदी भाषा सीखते थे। कुछ स्कूल और कॉलेज हिंदी के अतिरिक्त बोर्ड और निबंध बोर्ड में निबंध लेखन का आयोजन करते हैं, छात्रों को बोर्ड परीक्षा में हिंदी निबंध लिखने की आवश्यकता होती है।"
+
+    return translated_text
+
+
+# def generate_pdf():
+#     pdfkit.from_string(translate("hello"), "./out/out.pdf")
+#     return "PDF generated"
+
+
+@app.route("/")
+def generate_pdf():
+    translated_text = translate("hello")
+
+    html_content = (
+        """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <style>
+            /* Specify the font for Hindi text */
+            body {
+            font-family: Arial, sans-serif;
+            font-size: 22px;
+            lang: hi;
+            line-height: 1.5;
+            }
+        </style>
+        </head>
+        <body>
+        <p>
+        """
+        + translated_text
+        + """
+        </p>
+        </body>
+        </html>
+        """
+    )
+
+    options = {
+        "page-size": "Letter",
+        "encoding": "UTF-8",
+        "no-outline": None,
+    }
+
+    pdfkit.from_string(html_content, "./out/out.pdf", options=options)
+    return "PDF generated"
+
+
+# def generate_pdf():
+#     document = docx.Document()
+#     document.add_paragraph(translate("dummy"))
+#     document.save("test.docx")
+# * In this approach docx file is generated but it is not converted to pdf. Because docx2pdf only works in Microsoft Windows
+#     convert("test.docx")
 
 
 @app.route("/upload", methods=["POST"])
@@ -42,7 +110,11 @@ def fileUpload():
     print("File destination is: ", destination)
     file.save(destination)
 
-    read_file(destination)
+    text = read_file(destination)
+
+    # now translate the text and generate pdf
+    translated_text = translate(text)
+    # generate_pdf(translated_text)
 
     # session["uploadFilePath"] = destination
     response = "File successfully uploaded"
