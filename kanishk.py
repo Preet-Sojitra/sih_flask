b1fbc76839fad53365b2e3515f546a51f08014ce
Preet Sojitra <preet.dev373@gmail.com>
2023-10-09 23:27:56 +0530

    feat: add api for new model

--- kanishk.py
+++ kanishk.py
@@ -5,10 +5,10 @@ from transformers import MBart50TokenizerFast
 app = Flask(__name__)
 
 # Load the saved model and tokenizer
-with open("mbart_model.pkl", "rb") as model_file:
+with open("naya_model/mbart_model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 
-with open("mbart_tokenizer.pkl", "rb") as tokenizer_file:
+with open("naya_model/mbart_tokenizer.pkl", "rb") as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
 
 # Define the source language (English)
@@ -16,22 +16,30 @@ src_lang = "en_XX"
 
 # Define the target languages and their language codes
 tgt_languages = {
-    "Hindi": "hi_IN",
+    # "Hindi": "hi_IN",
+    # "Marathi": "mr_IN",
     "Gujarati": "gu_IN",
-    "Bengali": "bn_IN",
-    "Tamil": "ta_IN",
-    "Telugu": "te_IN",
+    # "Bengali": "bn_IN",
+    # "Tamil": "ta_IN",
+    # "Telugu": "te_IN",
+    # "Urdu": "ur_PK",
 }
+
 
 @app.route("/", methods=["GET"])
 def home():
     return "Translation API is up and running!"
+
 
 @app.route("/translate", methods=["POST"])
 def translate():
     input_data = request.get_json()
     input_text = input_data.get("text")
     target_language = input_data.get("target_language")
+    print(input_data)
+    print(input_text)
+    print(target_language)
+    print(tgt_languages[target_language])
 
     if not input_text or target_language not in tgt_languages:
         return jsonify({"error": "Invalid input"}), 400
@@ -39,13 +47,15 @@ def translate():
     lang_code = tgt_languages[target_language]
     model_inputs = tokenizer(input_text, return_tensors="pt")
     generated_tokens = model.generate(
-        **model_inputs,
-        forced_bos_token_id=tokenizer.lang_code_to_id[lang_code]
+        **model_inputs, forced_bos_token_id=tokenizer.lang_code_to_id[lang_code]
     )
-    translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+    translated_text = tokenizer.batch_decode(
+        generated_tokens, skip_special_tokens=True
+    )[0]
 
+    # return jsonify({"translation": translated_text})
     return jsonify({"translation": translated_text})
+
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
-
